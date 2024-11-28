7eb766e9d1febc57a76bbe22307ad42415729728
Shorts71 <tylerly385@gmail.com>
2024-11-27 21:06:08 -0500

    Finished Part 1 of Assignment 3

--- Part1/script.js
+++ Part1/script.js
@@ -1,15 +1,37 @@
 const $ = selector => document.querySelector(selector);
+$("#subtotal").focus();
 
-const CalculateTax = evt => {
-    if ($("#subtotal").value == "" || $("#taxrate").value == "") { 
-        alert("One of the required fields is missing.");
-    } else {
+const Calculate = evt => {
         const Subtotal = parseFloat($("#subtotal").value);
-        const TaxRate = parseFloat($("#taxrate").value);
+        const TaxRate = parseFloat($("#taxrate").value / 100);
+        if (isNaN(Subtotal) || isNaN(TaxRate)) { 
+            alert("One of the required fields is missing.");
+            return;
+        }
+        if (0 > Subtotal || 10000 < Subtotal) {
+            alert("Please enter a subtotal between $0 and $10000.");
+            return;
+        } 
+        if (0 > TaxRate || 0.12 < TaxRate) {
+            alert("Please enter a tax rate between 0 and 12 percent.");
+            return;
+        }  
         const SalesTax = Subtotal * TaxRate;
-        $("#salestax").textContent = SalesTax.toFixed(2);
-    }
+        const Total = Subtotal + SalesTax;
+        $("#salestax").value = SalesTax.toFixed(2);
+        $("#total").value = Total.toFixed(2);
+        $("#subtotal").focus();
 }
 document.addEventListener("DOMContentLoaded", ()=>{
-    $("#Calculate").addEventListener("click", CalculateTax);
+    $("#Calculate").addEventListener("click", Calculate);
+});
+
+const Clear = evt => {
+    $("#subtotal").value = "";
+    $("#taxrate").value = "";
+    $("#salestax").value = "";
+    $("#total").value = "";
+}
+document.addEventListener("DOMContentLoaded", ()=>{
+    $("#Clear").addEventListener("click", Clear)
 });
